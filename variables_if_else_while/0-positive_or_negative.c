9bda8f1c4a8b0cb93c0416e08c74074b129242e5
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-17 12:48:35 +0000

    update file 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n>0)
 	{	
-		printf("is positive")
-	};
+		printf("is positive");
+	}
 	else if (n < 0)
 	{
-		printf("is negative")
-	};
+		printf("is negative");
+	}
 	else
 	{
-		printf("is zero")
-	};
+		printf("is zero");
+	}
 	return (0);
 }
