923df776754f11d8732093fa7a99efe12751a2b3
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-18 11:20:28 +0000

    update file 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,18 @@
-#
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
