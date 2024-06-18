2b0c691ad2153fbcd1dae32887c513940bd7a2d8
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-18 08:00:12 +0000

    update file 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int main(void)
 	printf("Last digit of %d is %d ", n, lastdigit );
 	if (lastdigit > 5)
 	{
-		printf("and is greater than 5");
+		printf("and is greater than 5\n");
 	}
 	else if (lastdigit == 0)
 	{
-		printf("and is 0");
+		printf("and is 0\n");
 	}
 	else if (lastdigit < 6 && lastdigit != 0)
 	{
-		printf("and is less than 6 and not 0");
+		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
 }
