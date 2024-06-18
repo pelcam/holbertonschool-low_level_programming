318f9542477ea1ada7fc326f591a1d8003ea1d1b
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-18 07:35:14 +0000

    update file 1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d")
+	if (n > 5)
+	{
+		printf("and is greater than 5")
+	}
+	else if (n == 0)
+	{
+		printf("and is 0")
+	}
+	else if (0 < n < 6)
+	{
+		printf("and is less than 6 and not 0")
+	}
+	return (0);
+}
