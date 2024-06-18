f3d41a8d8874ee6cc392eec1d9d0bdc34dc99fd7
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-18 11:31:23 +0000

    update file 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int hexa;
+
+	for (hexa = 0; hexa < 15; hexa++)
+	{
+		printf("%x", hexa);
+	}
+	printf('\n')
+	return (0);
+}
