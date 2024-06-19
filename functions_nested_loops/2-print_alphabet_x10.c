fc04455a730065c8ac269735efc628a8fa0d94de
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-19 10:08:46 +0000

    update file 0

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * print_alphabet_x10 - Print : the alphabet ten time
+ * Return: 0
+ */
+
+void print_alphabet(void);
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
+
+void print_alphabet_x10(void);
+{
+	while (print_alphabet < '10')
+		print_alphabet;
+}
