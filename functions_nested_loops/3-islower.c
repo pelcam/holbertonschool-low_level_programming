5e453d9f7075aefa286ef99f097f326e5127246e
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-19 13:42:17 +0000

    update file 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase character
+ * Return: 0
+ */
+
+int _islower(int c);
+{
+	char = 'c'
+
+	if (_islower('c') != 0)
+		_putchar(1)
+	else
+		_putchar(0)
+}
