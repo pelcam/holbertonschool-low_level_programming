63ce34fc49c71f7b13cc667e35a8e24a0b80c157
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-20 12:16:54 +0000

    update file 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - lalala
+ * @n:  some variable
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	int d;
+	d = n % 10;
+
+	_putchar(d);
+	return (0);
+}
