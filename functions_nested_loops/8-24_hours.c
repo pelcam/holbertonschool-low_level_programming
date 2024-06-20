5fdaccc8a02d4e2d92cc88a4d6344cb3a5d0f972
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-20 21:31:08 +0000

    update file 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,8 +1,28 @@
 #include "main.h"
 
 /**
+ * jack_bauer - prints every minute of the day of Jack Bauer
  */
 
 void jack_bauer(void)
 {
+	int dh, h, dm, m;
+
+	for (dh = 0; dh <= 2; dh++)
+	{
+		for (h = 0; h <= 9; h++)
+		{
+			for (dm = 0; dm <= 5; dm++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					_putchar(dh);
+					_putchar(h);
+					_putchar(dm);
+					_putchar(m);
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
