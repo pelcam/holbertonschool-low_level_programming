66b399395541a2f8d61acd40e6ceec945962894a
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-20 11:30:35 +0000

    update file 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,12 +2,13 @@
 
 /**
  * _isalpha - check  if c is a letter, lowercase or uppercase
+ * @c: variable to print
  * Return: 0
  */
 
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
