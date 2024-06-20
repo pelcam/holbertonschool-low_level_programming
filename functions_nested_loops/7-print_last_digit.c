6ba0f91b097b1bc780e9611c28fbb1ecf6583e85
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-20 12:37:45 +0000

    update file 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_last_digit - lalala
- * @n:  some variable
+ * print_last_digit - prints the last digit of a number
+ * @n: initial number
  * Return: 0
  */
 
@@ -11,6 +11,11 @@ int print_last_digit(int n)
 	int d;
 
 	d = n % 10;
+
+	if (d < 0)
+		d = -d;
+
+	_putchar(d + '0');
 
 	return (d);
 }
