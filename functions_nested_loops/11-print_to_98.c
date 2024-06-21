c636bac9461184ea86560291a57618f64b25340a
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-21 11:51:54 +0000

    update file 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,12 +12,12 @@ void print_to_98(int n)
 	{
 		if (n > 98)
 		{
-			printf("%d", n);
+			printf("%d, ", n);
 			n--;
 		}
 		else
 		{
-			printf("%d", n);
+			printf("%d, ", n);
 			n++;
 		}
 	}
