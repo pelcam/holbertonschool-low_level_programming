424d86aef51df6ba27aadfb16133c42181f139ca
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-21 08:49:34 +0000

    update file 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,21 +2,29 @@
 
 /**
  * times_table - prints the nine times table, starting with zero
- * Return: 0
  */
 
 void times_table(void)
 {
-	int f1, f2, prd;
+	int f1, f2, prd, u;
 
 	for (f1 = 0; f1 <= 9; f1++)
 	{
 		for (f2 = 0; f2 <= 9; f2++)
 		{
 			prd = f1 * f2;
-			_putchar(prd + '0');
-			if (f2 < 9)
+			if (prd <= 9)
 			{
+				_putchar(prd + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			if (prd >= 10)
+			{
+				u = prd % 10;
+				_putchar(prd / 10);
+				_putchar(u);
 				_putchar(',');
 				_putchar(' ');
 			}
