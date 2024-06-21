c1afe304f56b88d684c4dc1e3452c56e6d660d14
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-21 10:53:42 +0000

    update file 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: r
+ */
+
+int add(int a, int b)
+{
+	int r;
+
+	r = a + b;
+
+	return (r);
+}
