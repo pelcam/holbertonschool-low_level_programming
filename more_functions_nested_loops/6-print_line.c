4b0c69c1b3fe673bd8753eeca37b0f7a16705997
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-24 12:51:17 +0000

    update file 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,9 +9,18 @@ void print_line(int n)
 {
 	int c;
 
-	for (c = 0; c <= n; c++)
+	c = 0;
+
+	if (c == 0)
 	{
-		_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (c = 0; c <= n; c++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
