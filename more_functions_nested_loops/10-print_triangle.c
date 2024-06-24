cb9c1fbad3a1cd16ff9c3b2c229a6a5a12b5faef
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-24 16:14:47 +0000

    update file 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle, followed by a new line
+ * @size: size of the triangle printed
+ */
+
+void print_triangle(int size)
+{
+	int l, e, t;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (l = 0; l <= size; l++)
+		{
+			for (e = size; e >= size; e--)
+			{
+				_putchar(' ');
+			}
+			for (t = 0; t <= size; t++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+}
