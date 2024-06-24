a05260aad245b4ac7ecc2e55c366b8f8372d00fe
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-24 13:27:13 +0000

    update file 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int h, w;
+
+	for (h = 0; h <= size; h++)
+	{
+		for (w = 0; w <= size; w++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
