1e0eb124b5bf1df61a7349ff1f1dbc4999977c3c
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-24 12:01:42 +0000

    update file 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first factor
+ * @b: second factor
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int p;
+
+	p = a * b;
+
+	_putchar(p);
+	return (0);
+}
