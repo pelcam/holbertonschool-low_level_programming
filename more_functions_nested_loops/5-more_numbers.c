cf1ccb71993aad1a0313d4101d0ae588ac225dfa
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-24 12:33:34 +0000

    update file 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers -  prints 10 times the numbers from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int n, r, d, u;
+
+	for (r = 0; r <= 10; r++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			d = n / 10;
+			u = n % 10;
+			if (n >= 10)
+				_putchar(d + '0');
+			_putchar(u + '0');
+		}
+		_putchar('\n');
+	}
+}
