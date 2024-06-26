8f0c595f5c83bd25a385df13070af300ad520493
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-26 20:37:03 +0000

    update file 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: printed string
+ */
+
+void print_rev(char *s)
+{
+	int a, r, l;
+
+	a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	l  = a;
+
+	for (r = l; r >= 0; r--)
+	{
+		_putchar(s[r]);
+	}
+
+	_putchar('\n');
+}
