396e36985f81ee1c88edb3810275fc04855b3395
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-26 10:58:03 +0000

    update file 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - print a string in stdout, followed by a new line
+ * @str: string to print
+ */
+
+void _puts(char *str)
+{
+	int my_char = 0;
+
+	while (str[my_char] != '\0')
+	{
+		_putchar(my_char);
+		my_char++;
+	}
+}
