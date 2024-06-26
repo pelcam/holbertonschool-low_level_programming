c55b7427b942a3b0f7f0e968c546553b00005ab0
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-26 09:06:58 +0000

    update file 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,16 @@
 /**
  * _strlen - returns the length of a string
  * @s: string to measure
+ * Return: count
  */
 
 int _strlen(char *s)
 {
 	int count = 0;
 
-	while (*s != '\n')
+	while (*s != '\0')
+	{
 		count++;
+	}
 	return (count);
 }
