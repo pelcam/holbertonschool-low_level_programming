0c304288dd13cf9fd7d5fdcbc0b508af436208fd
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-27 21:57:49 +0000

    update file 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,15 +9,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int *ptr_dest = dest;
+	int i = 0;
 
-	while (*src != '\0')
+	while (src[i] != '\0')
 	{
-		*ptr_dest = *src;
-		ptr_dest++;
-		src++;
+		dest[i] = src[i];
+		i++;
 	}
-	*ptr_dest = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
