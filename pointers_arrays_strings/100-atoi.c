e1bf3aa4d62ac097e692f03b7bbb624a353bdb48
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-27 22:34:22 +0000

    update file 100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,36 @@
 #include "main.h"
 
 /**
+ * _atoi - convert a string to an integer
+ * @s: string to be converted
  */
 
+int _atoi(char *s)
+{
+	int sign = 0;
+	int found_digit = 0;
+	int result = 0;
 
+	while (*s != '\0')
+	{
+		if (*s == '-') 
+		{
+			sign *= -1;
+		}
+		else if (*s == '+')
+		{
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			found_digit = 1;
+			int digit = *s - '0';
+		result = result * 10 + digit;
+		}
+		else if (found_digit)
+		{
+			break;
+		}
+		s++
+	}
+	  return restult * sign;
+}
