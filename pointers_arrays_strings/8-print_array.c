a0f23bcc0889f3975f2d915c2f7683259723b7b7
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-27 11:58:50 +0000

    update file 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - that prints n elements of an array of integers
+ * @a: array of integers
+ * @n: number of elements to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int c;
+
+	for (c = 0; a[c] <= n; c++)
+	{
+		printf("%d", a[c]);
+		_putchar(',');
+		_putchar(' ');
+	}
+}
