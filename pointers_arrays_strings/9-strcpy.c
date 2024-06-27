f199d17fb2a488fa82a292759b944a9e7b65bd79
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-27 12:55:30 +0000

    update file 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a pointed string to the buffer of another pointer
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return (dest);
+}
