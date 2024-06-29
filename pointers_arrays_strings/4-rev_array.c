91c20cc65626a9a814a80382bbc84bb080a93753
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-29 12:42:48 +0000

    update file 4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array to be reversed
+ * @n: number of element of the array
  */
 
+void reverse_array(int *a, int n)
+{
+	int i = 0, l, tmp;
 
+	for (l = n - 1; l > i; l--)
+	{
+		tmp = a[l];
+		a[l] = a[i];
+		a[i] = tmp;
+		i++;
+	}
+}
