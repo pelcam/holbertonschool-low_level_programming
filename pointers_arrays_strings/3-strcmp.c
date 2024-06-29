900fc7509e5bdc2f3d42e3890d725cc46a47d086
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-29 12:37:12 +0000

    update file 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
+	while (*s1 && (*s1 == *s2))
 	{
 		s1++;
 		s2++;
 	}
-	if (*s1)
-		s1++;
 	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
