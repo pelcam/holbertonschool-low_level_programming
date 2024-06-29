37b72b0f86b9b2c7d9eeb88a9cf769078e5a6c28
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-29 12:32:05 +0000

    update file 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
 /**
+ * _strcmp - compares two strings
+ * @s1: first string to be compared
+ * @s2: second string to be commpared
+ * Return: 
  */
 
-
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
