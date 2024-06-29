53519434fc918e8b378c2290507e268c413a4410
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-29 14:12:21 +0000

    update file 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * cap_string - capitalizes all words of a string
+ * @sptr: string pointer
+ * Return: sptr
  */
 
+char *cap_string(char *sptr)
+{
+	int c;
 
+	for (c = 0; sptr[c] '\0'; c++)
+	{
+		if (sptr[c] == ' ' || sptr[c] == '\t' || sptr[c] == '\n' || sptr[c] == ',' || sptr[c] == ';' sptr[c] == '.' || sptr[c] == '!' || sptr[c] == '?' || sptr[c] == '"' || sptr[c] == '(' || sptr[c] == ')' || sptr[c] == '{' || sptr[c] == '}')
+		{
+			a++;
+			if (str[a] >= 'A' && str[a] <= 'Z')
+				a++;
+			else if (str[a] >= 'a' && str[a] <= 'z')
+				str[a] = str[a] - 32;
+			a--;
+		}
+		else if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] = str[0] - 32;
+	}
+	return (str);
+}
