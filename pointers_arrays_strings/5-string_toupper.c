546b01b8eb45400da4e54f00464112361d38dfd3
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-29 12:48:20 +0000

    update file 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
 /**
+ * *string_toupper - changes all lowercase letters of a string to uppercase.
+ * @sptr: string pointer
+ * Return: sptr
  */
 
-
+char *string_toupper(char *sptr)
+{
+}
