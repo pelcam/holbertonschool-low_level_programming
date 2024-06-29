770fc6120a8b7a44e0e54d7ea85d8c1b96e73d61
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-06-29 12:16:07 +0000

    update file 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strncpy - copie a string
+ * @dest: where to copie the string
+ * @src: string to be copied
+ * @n: variable integer
+ * Return: dest
  */
 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i1 = 0, i2 = 0;
 
+	while (a < n && src[a] != '\0')
+		a++;
+	for (; b < a && src[b] != '\0'; b++)
+	{
+		dest[b] = src[b];
+	}
+	return (dest);
+}
