3f9ff3f1d829d66e3216359b843ae847a8557f1e
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-03 19:49:23 +0000

    update file 8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -11,10 +11,10 @@ void print_diagsums(int *a, int size)
 {
 	int i, s1 = 0, s2 = 0;
 
-	for (i = 0; i < size; i++) 
+	for (i = 0; i < size; i++)
 	{
-		s1 += ;
-		s2 += ;
+		s1 += a[i * size + i];
+		s2 += ;[i * size (size - i)];
 	}
 	printf("%d, %d \n", s1, s2);
 }
