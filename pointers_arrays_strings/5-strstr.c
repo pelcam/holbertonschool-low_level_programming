e9a04ced0496a74c377048c78141f313b1b1fdf8
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-03 12:35:12 +0000

    update file 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: where to find the occurence of the substring
+ * @needle: occurence to find
+ * Return:
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+}
