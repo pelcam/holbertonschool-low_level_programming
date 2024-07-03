0d40e99903fed2d17337a9a9f0a73f953f46c030
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-03 19:13:03 +0000

    update file 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,4 +9,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	char *h;
+	char *n;
+
+	while (*haystack)
+	{
+		h = haystack;
+		n = needle;
+
+		while (*h && *n && (*h == *n))
+		{
+			h++;
+			n++;
+		}
+		 if (*n == '\0')
+			 return (haystack);
+		 haystack++;
+	}
+	return ("NULL");
 }
