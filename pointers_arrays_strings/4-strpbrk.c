9bab6038f3acb1873e88fc96838280aa8fa0ea6f
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-03 09:50:52 +0000

    update file 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string
+ * @accept: accepted characters
+ * Return:
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+}
