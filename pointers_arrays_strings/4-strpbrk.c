f9f013b73792fe49c5f294265232c40e0698a8eb
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-03 11:56:33 +0000

    update file 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -2,11 +2,29 @@
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
- * @s: string
- * @accept: accepted characters
- * Return:
+ * @s: substring
+ * @accept: characters accepted
+ * Return: c
  */
 
 char *_strpbrk(char *s, char *accept)
 {
+	unsigned int c = 0;
+	int f;
+	char *a;
+
+	while (*s)
+	{
+		for (a = accept; *a; a++)
+		{
+			f = 0;
+			if (*s == *a)
+			{
+				f = 1;
+			}
+		}
+		c++;
+		s++;
+	}
+	return (a);
 }
