3ad849415b7c26768e87bcf7d270c58f9d11ee15
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-03 19:47:38 +0000

    update file 8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix of integers
+ * @a: cube of integers
+ * @size: size of the cube
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++) 
+	{
+		s1 += ;
+		s2 += ;
+	}
+	printf("%d, %d \n", s1, s2);
+}
