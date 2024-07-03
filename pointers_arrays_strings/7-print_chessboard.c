7e7c141e7e49635d9345acbda17f44570da54c69
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-03 19:36:38 +0000

    update file 7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,10 +2,19 @@
 
 /**
  * print_chessboard - prints the chessboard
- * @a:
- * Return:
+ * @a: the chessboard to be printed
  */
 
 void print_chessboard(char (*a)[8])
 {
+	int l, c;
+
+	for (l = 0; l < 8; l++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[l][c]);
+		}
+		_putchar('\n');
+	}
 }
