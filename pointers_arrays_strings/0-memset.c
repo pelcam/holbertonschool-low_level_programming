55acbfb31aa1a88a183fa2c0008a103c15619ec8
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-03 07:34:31 +0000

    update file 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -3,10 +3,17 @@
 /**
  * _memset - fills memory with a constant byte
  * @s: pointer to the memory area
- * @b: constant byte
+ * @b: constant byte to fill the memory with
  * @n: number of first bytes to fill
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
