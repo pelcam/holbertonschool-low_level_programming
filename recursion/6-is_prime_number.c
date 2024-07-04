c7fe722aad19b4b29e0243d980f0ec283185b0e6
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-04 14:25:16 +0000

    update file 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
 /**
- * is_prime_number - returns 1 if the input integer is a prime number
- * @n:
+ * is_prime_helper
  */
 
-int is_prime_number(int n)
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * @n: 
+ */
+
+int is_prime_number(int n, int div)
 {
+	return is_prime_helper(n, 2);
 }
