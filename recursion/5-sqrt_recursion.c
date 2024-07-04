bc3d507c50c8578f88da566115897bab813830a3
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-04 11:48:08 +0000

    update file 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number we want the natural root from
+ * Return: natural square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+}
