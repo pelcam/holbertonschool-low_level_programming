d1d557f73de28b0d28e8886d1667a2019b22f0cf
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-05 02:51:06 +0000

    update file 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,9 +1,36 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>i
 
 /**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
  */
 
-int main()
+int main(int argc, char *argv[])
 {
+	int i, j;
+	int s = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < rgc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		s += atoi(argv[i]);
+	}
+	printf("%d\n", s);
+	return (0);
 }
