5394334cbde503478e2546283ebf900834b83068
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-10 20:26:28 +0000

    update file 4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,14 +1,23 @@
 #include "main.h"
-#include <sdtio.h>
+#include <stdlib.h>
 
 /**
- * free_grid -
- * @grid:
- * @height:
- *
- * Return:
+ * free_grid - frees a 2 dimensional grid
+ * @grid: the array to free
+ * @height: the height of the array
  */
-
 void free_grid(int **grid, int height)
 {
+    int i;
+
+    if (grid == NULL || height <= 0)
+        return;
+
+    for (i = 0; i < height; i++)
+    {
+        free(grid[i]);
+    }
+
+    free(grid);
 }
+
