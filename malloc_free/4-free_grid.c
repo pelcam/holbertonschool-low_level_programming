1199e8dc9df0a4da98bf638af1976998e1992899
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-10 20:28:55 +0000

    update file 4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -8,16 +8,16 @@
  */
 void free_grid(int **grid, int height)
 {
-    int i;
+	int i;
 
-    if (grid == NULL || height <= 0)
-        return;
+	if (grid == NULL || height <= 0)
+		return;
 
-    for (i = 0; i < height; i++)
-    {
-        free(grid[i]);
-    }
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
 
-    free(grid);
+	free(grid);
 }
 
