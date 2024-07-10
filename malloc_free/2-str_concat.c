7c4334af6c25176d7f55ed2bd7112b8bcbf80f3d
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-10 17:47:31 +0000

    files 2 saved

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string number one
+ * @s2: string number two
+ *
+ * Return: NULL on failure, or the concatenated strings
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char
+}
