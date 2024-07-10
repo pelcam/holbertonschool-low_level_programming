c75de03d5d43b3a91f756ef353e46f2218d4089f
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-10 20:12:23 +0000

    update file 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid -
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
  * @width: array width
  * @height: array height
  *
@@ -11,4 +11,37 @@
 
 int **alloc_grid(int width, int height)
 {
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int **)malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
 }
