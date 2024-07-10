c00db58bcb3e125716a0408fdb8afd05a068f2fb
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-10 17:04:51 +0000

    update files 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: string given as parameter
+ *
+ * Return: a pointer to a newly allocated space in memory, NULL if str = NULL
+ */
+
+char *_strdup(char *str)
+{
+}
