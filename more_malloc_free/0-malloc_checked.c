e2526d891967bfb67844f3963b60282424b354d0
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-11 09:12:39 +0000

    update file 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,9 +3,17 @@
 
 /**
  * malloc_checked - allocates memory using malloc
- * @b:
+ * @b: number of characters to allocate memory for
+ *
+ * Return: a pointer to the allocated memory, and NULL if failed allocation
  */
 
 void *malloc_checked(unsigned int b)
 {
+	int *ptr;
+
+	ptr = (int *)malloc(b * sizeof(unsigned int));
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
 }
