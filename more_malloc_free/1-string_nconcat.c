bcd3dbaa1fa3b3e3f60f0119ad61f4b7ebde23ec
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-11 12:27:23 +0000

    update file 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: 
+ *
+ * Return:
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+}
