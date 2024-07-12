2fa845aa86f6b8c8953d8604e66894c0679eba49
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-12 16:40:16 +0000

    update file 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,4 +12,25 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *concat;
+	unsigned int l1 = 0, l2 = 0, i, j;
+
+	if (s1 != NULL)
+		while (s1[l1] != '\0')
+			l1++;
+	if (s2 != NULL)
+		while (s2[l2] != '\0')
+			l2++;
+	if (n >= l2)
+		n = l2;
+	concat = malloc((l1 + n + 1) * sizeof(char));
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < l1; i++)
+		concat[i] = s1[i];
+	for (j = 0; j < n; j++)
+		concat[i + j] = s2[j];
+	concat[i + j] = '\0';
+	return (concat);
 }
