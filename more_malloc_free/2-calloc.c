2672ec705be3c83cdab884024a468eb6ccbcf04d
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-12 16:49:00 +0000

    update file 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,8 +2,26 @@
 #include <sdtlib.h>
 
 /**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements in the array
+ * @size: size of the array
+ *
+ * Return: a pointer to the allocated memory
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr;
+	unsigned int i;
+	char *char_ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	char_ptr = (char *)ptr;
+	for (i = 0; i < (nmemb * size); i++)
+		char_ptr[i] = 0;
+	return (ptr);
 }
