c51761f03357dc169498058cbad2459c45c41754
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-12 18:37:27 +0000

    update file 1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,7 +8,6 @@
  * @age: The age of the dog
  * @owner: The name of the dog's owner
  */
-
 struct dog
 {
 	char *name;
@@ -16,6 +15,13 @@ struct dog
 	char *owner;
 };
 
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to the struct dog to initialize
+ * @name: The name to set
+ * @age: The age to set
+ * @owner: The owner to set
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
