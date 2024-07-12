6c4b58d809821ae32b9ff45ee40cbdc8d161f3ca
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-12 17:36:23 +0000

    update header file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {char *name;
-       	float age;
-       	char *owner;
+/**
+ * struct dog - A structure to represent a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
-
