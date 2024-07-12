6154745c891b85898a528f1cd005a88736330135
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-12 17:01:54 +0000

    update file 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -5,7 +5,7 @@
  * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n: 
+ * @n: numbr of btytes to concatenate
  *
  * Return: a pointer to a newly allocated space in memory, with s1 and s2
  */
