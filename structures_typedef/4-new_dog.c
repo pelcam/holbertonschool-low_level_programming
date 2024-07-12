be41dd3adc33322ecccd0f04e79da718c2fb1e2d
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-12 18:48:31 +0000

    update file 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,1 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
 #include "dog.h"
+
+/**
+ * new_dog - Creates a new dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the dog's owner
+ * 
+ * Return: Pointer to the new dog, or NULL on failure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    dog_t *d;
+    char *name_copy;
+    char *owner_copy;
+
+    d = malloc(sizeof(dog_t));
+    if (d == NULL)
+        return (NULL);
+
+    name_copy = malloc(strlen(name) + 1);
+    if (name_copy == NULL)
+    {
+        free(d);
+        return (NULL);
+    }
+    strcpy(name_copy, name);
+
+    owner_copy = malloc(strlen(owner) + 1);
+    if (owner_copy == NULL)
+    {
+        free(name_copy);
+        free(d);
+        return (NULL);
+    }
+    strcpy(owner_copy, owner);
+
+    d->name = name_copy;
+    d->age = age;
+    d->owner = owner_copy;
+
+    return (d);
+}
