b9745d202c6f522ac991dc01d58a3fe22d61e1cb
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-12 16:29:21 +0000

    update file 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,15 +20,13 @@ char *str_concat(char *s1, char *s2)
 	if (s2 != NULL)
 		while (s2[l2] != '\0')
 			l2++;
-
-	cstr = (char *)malloc((l1 + l2) * sizeof(char));
+	cstr = (char *)malloc((l1 + l2 + 1) * sizeof(char));
 	if (cstr == NULL)
 		return (NULL);
-
 	for (i1 = 0; i1 < l1; i1++)
 		cstr[i1] = s1[i1];
 	for (i2 = 0; i2 < l2; i2++)
 		cstr[i2 + i1] = s2[i2];
-
+	cstr[i1 + i2] = '\0';
 	return (cstr);
 }
