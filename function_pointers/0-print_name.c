7bc96b8dc6cc279d2f788ecae90c9e28a5034129
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-15 17:32:21 +0000

    update file 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,9 +1,15 @@
-#include "main.h"
-#include <>
+#include "function_pointers.h"
 
 /**
+ * print_name - prints a name
+ * @name: pointer to the name to be printed
+ * @f: pointer to a function
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
