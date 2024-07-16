997b89b6d96a8eb445412c4bd6d03f7d483f1026
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-16 09:14:01 +0000

    update file 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,12 +1,17 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator -
- * @array:
- * @size:
- * @action:
+ * array_iterator - executes a function given as a parameter,
+ * on each element of an array
+ * @array: pointer to the array to be treated
+ * @size: size of the array
+ * @action: action to execute on the elements of the array
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	long unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
