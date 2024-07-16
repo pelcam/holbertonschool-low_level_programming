1dfc113e6df155419716458bfe08e0b26ca74965
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-16 12:09:47 +0000

    update file 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search integers from
+ * @size: size of the array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: the index of the first element,
+ * for which the cmp function does not return 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+		if (cmp != 0)
+			return(i);
+	}
+	return (-1);
+}
