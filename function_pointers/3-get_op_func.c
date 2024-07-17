cc8044725836dd0bec28d33c166606b3283ef8fc
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-17 12:21:52 +0000

    update files from task3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -16,5 +16,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -46,8 +46,10 @@ int op_mul(int a, int b)
 int op_div(int a, int b)
 {
 	if (b == 0)
+	{
 		printf("Error\n");
 		exit(100);
+	}
 	else
 		return (a / b);
 }
@@ -62,8 +64,10 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b == 0)
+	{
 		printf("Error\n");
 		exit(100);
+	}
 	else
 		return (a % b);
 }
