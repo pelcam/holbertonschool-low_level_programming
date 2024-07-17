8b21b02108afb4a81b26d31f51bbc5ba11ce64ae
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-17 08:59:13 +0000

    update file 3 functions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,6 +1,56 @@
 #include "3-calc.h"
 
 /**
+ * op_add - returns the sum of a and b
+ * @a: first number
+ * @b: number to add
+ *
+ * Return: the sum of a and b
  */
+int op_add(int a, int b)
+{
+}
 
+/**
+ * op_sub - returns the difference of a and b
+ * @a: first number
+ * @b: number to substract
+ *
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+}
 
+/**
+ * op_mul - returns the product of a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+}
+
+/**
+ * op_div - returns the result of the division of a by b
+ * @a: number to divide
+ * @b: number to divide by
+ *
+ * Return: the result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+}
+
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * @a: number to divide
+ * @b: number to divide by
+ *
+ * Return: returns the remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+}
