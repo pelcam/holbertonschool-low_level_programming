fb0df331a0be983d66c7fbea21c7c654bc8194f3
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-18 12:45:39 +0000

    update file 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,15 +2,24 @@
 
 /**
  * sum_them_all - returns the sum of all its parameters
- * @n: count of remaining arguments 
+ * @n: number of given arguments 
  *
  * Return: the sum of all its parameters, and 0 if n is null
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum;
+	va_list list;
+	int i;
+	int sum = 0;
 
+	for(i = 0, i < n, i++)
+	{
+		va_arg(n, list);
+		sum += 
+	}
 	if (n == 0)
 		return (0);
+	va_end(list);
+	return (sum);
 }
