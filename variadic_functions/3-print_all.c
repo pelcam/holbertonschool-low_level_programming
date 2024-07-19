fc652c1da3acb745f9d7cde87de664edeb9f6648
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-19 15:14:55 +0000

    update files 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,8 @@
 #include "variadic_functions.h"
 
 /**
+ * print_all -
+ * @format:
  */
 
 void print_all(const char * const format, ...)
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stddef.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
