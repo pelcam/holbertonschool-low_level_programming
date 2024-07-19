da7e8f09d593f086d1c91bfc06e298c0a4b3ce11
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-19 15:09:07 +0000

    update file 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -5,20 +5,26 @@
  * @separator: string to be printed between the strings
  * @n: number of arguments
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i;
+	char *str;
 
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
+		str = va_arg(list, char*);
+		if (str == NULL)
+		{
+			str = "(nil)";
+		}
+
 		if (i != 0 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
-		printf("%d", va_arg(list, int));
+		printf("%s", str);
 	}
 	printf("\n");
 	va_end(list);
