bf4ad6ef690203e0f1c2e1b8089950b12faf32d1
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-07-19 15:30:20 +0000

    update file 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,10 +1,38 @@
 #include "variadic_functions.h"
 
-/**
- * print_all -
- * @format:
- */
-
 void print_all(const char * const format, ...)
 {
+	va_list args;
+	unsigned int i = 0;
+	char *str, *sep = "";
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		sep = ", ";
+		i++;
+	}
+	printf("\n");
+	va_end(args);
 }
