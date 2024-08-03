a4d57facf83e6835b48348cadf2319345366e9a2
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-03 10:01:03 +0000

    correcting file 1 betty style

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 /**
  * list_len - Returns the number of elements in a linked list_t list.
  * @h: Pointer to the head of the list.
- * 
+ *
  * Return: The number of elements in the list.
  */
 size_t list_len(const list_t *h)
