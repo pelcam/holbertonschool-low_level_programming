84e5c405359fde89212d4d750e54a3d8aa490b6e
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-03 09:49:37 +0000

    update file 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,10 +2,23 @@
 
 /**
  * print_list - prints all the elements of a list_t list
- * @h:
+ * @h: pointer to the head of the list
+ *
  * Return: the number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+	return n;
 }
