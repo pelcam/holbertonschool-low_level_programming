456135a44a3ee89acf75f3e359de5dda21f1abe1
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-04 08:49:41 +0000

    updating file 2 and 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
+ * add_node_end - Adds a new node at the end of a list_t list.
  * @head: Double pointer to the head of the list.
  * @str: String to be duplicated for the new node.
  *
  * Return: Address of the new element, or NULL if it failed.
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new_node, *current;
 
 	if (str == NULL)
 		return (NULL);
@@ -29,8 +29,18 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[new_node->len])
 		new_node->len++;
 
-	new_node->next = *head;
-	*head = new_node;
+	new_node->next = NULL;
 
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new_node;
+	}
 	return (new_node);
 }
