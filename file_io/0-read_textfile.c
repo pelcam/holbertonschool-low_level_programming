38afd1db1208d6ad0924820ffc5428df5b264cdd
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-09 15:52:02 +0000

    correcting file for betty style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -18,15 +18,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
 		return (0);
-	}
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
